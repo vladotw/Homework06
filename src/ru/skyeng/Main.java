e4787330b3dfc32d2c5ca8e9a983f2d42c1a5632
Vlad <vladot@ya.ru>
2022-12-07 09:11:28 +0600

    Task1

--- src/ru/skyeng/Main.java
+++ src/ru/skyeng/Main.java
@@ -2,6 +2,81 @@ package ru.skyeng;
 
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+    }
 
+    public static void task1() {
+
+        System.out.println("Задача_1");
+        System.out.println();
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task2() {
+
+        System.out.println("Задача_2");
+        System.out.println();
+
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task3() {
+
+        System.out.println("Задача_3");
+        System.out.println();
+
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task4() {
+
+        System.out.println("Задача_4");
+        System.out.println();
+
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task5() {
+
+        System.out.println("Задача_5");
+        System.out.println();
+
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task6() {
+
+        System.out.println("Задача_6");
+        System.out.println();
+
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task7() {
+
+        System.out.println("Задача_7");
+        System.out.println();
     }
 }
