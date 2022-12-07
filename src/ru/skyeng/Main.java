36f3072e31ed683a3559ebe4508b7b5cb7e5f986
Vlad <vladot@ya.ru>
2022-12-07 10:04:16 +0600

    Task1, Task2, Task3, Task4, Task5, Task6, Task7, Task8

--- src/ru/skyeng/Main.java
+++ src/ru/skyeng/Main.java
@@ -9,11 +9,14 @@ public class Main {
         task5();
         task6();
         task7();
+        task8();
+        task9();
+        task10();
     }
 
     public static void task1() {
 
-        System.out.println("Задача_1");
+        System.out.println("Задача 1");
         System.out.println();
 
         for (int i = 1; i < 10; i++) {
@@ -26,7 +29,7 @@ public class Main {
 
     public static void task2() {
 
-        System.out.println("Задача_2");
+        System.out.println("Задача 2");
         System.out.println();
 
         for (int i = 10; i > 1; i--) {
@@ -39,7 +42,7 @@ public class Main {
 
     public static void task3() {
 
-        System.out.println("Задача_3");
+        System.out.println("Задача 3");
         System.out.println();
 
         for (int i = 0; i < 17; i = i + 2) {
@@ -52,7 +55,7 @@ public class Main {
 
     public static void task4() {
 
-        System.out.println("Задача_4");
+        System.out.println("Задача 4");
         System.out.println();
 
         for (int i = 10; i > -10; i--) {
@@ -65,7 +68,7 @@ public class Main {
 
     public static void task5() {
 
-        System.out.println("Задача_5");
+        System.out.println("Задача 5");
         System.out.println();
 
         for (int i = 1904; i < 2096; i = i + 4) {
@@ -78,7 +81,7 @@ public class Main {
 
     public static void task6() {
 
-        System.out.println("Задача_6");
+        System.out.println("Задача 6");
         System.out.println();
 
         for (int i = 7; i < 100; i = i + 7) {
@@ -91,11 +94,53 @@ public class Main {
 
     public static void task7() {
 
-        System.out.println("Задача_7");
+        System.out.println("Задача 7");
         System.out.println();
 
         for (int i = 1; i < 1000; i = i * 2) {
             System.out.println(i);
         }
+
+        System.out.println();
+        System.out.println();
     }
+
+    public static void task8() {
+
+        System.out.println("Задача 8");
+        System.out.println();
+
+        int monthlyAccumulation = 29_000;
+        int total = 0;
+
+        for (int i = 1; i <= 12; i++) {
+            total = total + monthlyAccumulation;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total +
+                    " рублей");
+        }
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task9() {
+
+        System.out.println("Задача 9");
+        System.out.println();
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task10() {
+
+        System.out.println("Задача 10");
+        System.out.println();
+
+        System.out.println();
+        System.out.println();
+    }
+
+
+
 }
